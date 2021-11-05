ce400241daa0ff82ee1e98b9e7df96d74e39a3a8
Oleg Bogdasarov <o.bogdasarov@gmail.com>
2021-11-05 16:07:54 +0300

    Лекция 4

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
-const path = require('path');
 const yargs = require("yargs");
 const {getFileNamesInDirectory, promptUser, showFileContents} = require('./utils');
 
-const CWD = process.cwd();
+const options = yargs
+  .usage("Usage: -p <path> -f <find>")
+  .option("p", { alias: "path", describe: "Path to file", type: "string", demandOption: false })
+  .option("f", { alias: "find", describe: "Find substring", type: "string", demandOption: false })
+  .argv;
 
+const CWD = options.path ?? process.cwd();
 (async () => {
     const filesInCwd = await getFileNamesInDirectory(CWD);
     const userInput = await promptUser(filesInCwd);
-    await showFileContents(path.join(CWD, userInput));
+    await showFileContents(userInput, options.find);
 })();
--- Lesson4/utils.js
+++ Lesson4/utils.js
@@ -1,21 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+const {Transform} = require('stream');
+const {EOL} = require('os');
+require('colors');
+const {green} = require("colors");
 
 const isFile = (filepath) => {
     return fs.lstatSync(filepath).isFile();
+}
+const isDir = (filepath) => {
+    return fs.lstatSync(filepath).isDirectory();
 }
 
 const getFileNamesInDirectory = async (directory) => {
     const itemsInDirectory = await new Promise((resolve) => {
         fs.readdir(directory, (err, data) => {
-            resolve(data);
+            resolve(data.map(p => path.join(directory, p)));
         });
     });
-
-    return itemsInDirectory.filter((data) => {
-        return isFile(path.join(directory, data));
-    })
+    let files = itemsInDirectory.filter((data) => {
+        return isFile(data);
+    });
+    const dirs = itemsInDirectory.filter((data) => {
+        return isDir(data);
+    });
+    for(let dir of dirs) {
+        const filesSubDir = await getFileNamesInDirectory(dir);
+        files = files.concat(filesSubDir);
+    }
+    return files;
 }
 
 const promptUser = async (choices) => {
@@ -31,11 +45,36 @@ const promptUser = async (choices) => {
     return result[optionKey];
 }
 
-const showFileContents = async (filepath) => {
+const getStrWithFindSubstring = (str, findString) => {
+    const pos= str.indexOf(findString);
+    if(pos < 0)
+        return null;
+    return str.substring(0, pos) + green(str.substring(pos, pos + findString.length)) + str.substring(pos + findString.length);
+}
+
+const showFileContents = async (filepath, findString) => {
+    console.log(findString);
     return new Promise((resolve) => {
+        const transform = new Transform({
+            transform(chunk, encoding, callback) {
+                const strArr = chunk.toString().split(EOL);
+                for(const str of strArr) {
+                    if (findString === undefined)
+                        this.push(str + EOL);
+                    else {
+                        const resultStr = getStrWithFindSubstring(str, findString);
+                        if(resultStr !== null)
+                            this.push(resultStr + EOL);
+                    }
+                }
+                callback();
+            }
+        });
+
         const stream = fs.createReadStream(filepath, 'utf-8');
         stream.on('end', resolve);
-        stream.pipe(process.stdout);
+        stream.pipe(transform).pipe(process.stdout);
+
     });
 }
 
